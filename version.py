b7fb3d827cecb24ffebd03121e101ede56cf890b
Vinícius Gregório <gsi.vinicius@gmail.com>
2026-05-30 17:53:46 -0300

    Bump version to 1.2.4

--- version.py
+++ version.py
@@ -1,12 +1,12 @@
 """Version information for Videoteka."""
 
-__version__ = "1.2.3"
-__version_info__ = (1, 2, 3)
+__version__ = "1.2.4"
+__version_info__ = (1, 2, 4)
 
 # Version components
 MAJOR = 1
 MINOR = 2
-PATCH = 3
+PATCH = 4
 
 def get_version():
     """Get the version string."""
